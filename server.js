06c261e3251a6198e2abdeac4daf0548a998bc64
Navya <navyarajgupta2006@gmail.com>
2026-08-18 22:44:03 +0530

    fix SMTP connection

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const dns = require('dns');
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port:587,
-  secure:true,
+  secure:false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
